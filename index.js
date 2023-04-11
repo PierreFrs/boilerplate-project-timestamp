ed278a9560fa5c61749ebe3d40c51fc6817d513e
Pierre <pierrefraisse90@gmail.com>
2023-04-11 20:44:19 +0200

    Full Project

--- index.js
+++ index.js
@@ -27,6 +27,37 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// Blank object to be used in the response
+let resObject = {};
+// Get request
+app.get("/api/:date", (req, res) => {
+  // Capturing the date input
+  let date = req.params.date;
+  // If its a Date string
+  if (/\d{5,}/.test(date)) {
+    date = parseInt(date);
+    resObject["unix"] = new Date(date).getTime();
+    resObject["utc"] = new Date(date).toUTCString();
+  // If it is a timestamp
+  } else {
+    resObject["unix"] = new Date(date).getTime();
+    resObject["utc"] = new Date(date).toUTCString();
+  }
+  // Error Handler
+  if (!resObject["unix"] || !resObject["utc"]) {
+    res.json({ error: "Invalid Date" });
+  }
+  // Rendering the date input as a string
+  res.json(resObject);
+});
+
+// Handling if there is no date on the date input
+app.get("/api/", (req, res) => {
+  resObject["unix"] = new Date().getTime();
+  resObject["utc"] = new Date().toUTCString();
+  res.json(resObject);
 });
